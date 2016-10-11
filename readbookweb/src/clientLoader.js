a8c18f08fde10550b425d3c526161eb70f5fe2a3
學學安 <polo13999@gmail.com>
2016-10-11 17:26:53 +0800

    update

---
+++ readbookweb/src/clientLoader.js
@@ -0,0 +1,33 @@
+
+// polyfills
+import 'babel-polyfill';
+import main from './client';
+
+function run() {
+  // Run the application when both DOM is ready and page content is loaded
+  if (['complete', 'loaded', 'interactive'].includes(document.readyState) && document.body) {
+    main();
+  } else {
+    document.addEventListener('DOMContentLoaded', main, false);
+  }
+}
+
+if (!global.Intl) {
+  // You can show loading banner here
+
+  require.ensure([
+    // Add all large polyfills here
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/cs.js',
+  ], require => {
+    // and require them here
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/cs.js');
+
+    run();
+  }, 'polyfills');
+} else {
+  run();
+}
---
+++ readbookweb/src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-shadow */
+
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { setLocale } from '../../actions/intl';
+
+function LanguageSwitcher({ currentLocale, availableLocales, setLocale }) {
+  const isSelected = (locale) => locale === currentLocale;
+  const localeDict = {
+    'en-US': 'English',
+    'cs-CZ': 'Česky',
+  };
+  const localeName = (locale) => localeDict[locale] || locale;
+  return (
+    <div>
+      {availableLocales.map(locale => (
+        <span key={locale}>
+          {isSelected(locale) ? (
+            <span>{localeName(locale)}</span>
+          ) : (
+            <a
+              href={`?lang=${locale}`}
+              onClick={(e) => {
+                setLocale({ locale });
+                e.preventDefault();
+              }}
+            >{localeName(locale)}</a>
+          )}
+          {' '}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+LanguageSwitcher.propTypes = {
+  currentLocale: PropTypes.string.isRequired,
+  availableLocales: PropTypes.arrayOf(PropTypes.string).isRequired,
+  setLocale: PropTypes.func.isRequired,
+};
+
+const mapState = (state) => ({
+  availableLocales: state.runtime.availableLocales,
+  currentLocale: state.intl.locale,
+});
+
+const mapDispatch = {
+  setLocale,
+};
+
+export default connect(mapState, mapDispatch)(LanguageSwitcher);
