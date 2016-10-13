d8dc503411d7e96fd88d81977a4c704f7f392b6b
Wolke <wolkesau@gmail.com>
2016-10-13 11:34:58 +0800

    Docker 跑 node tests.js 已可以實作 gzip on text/html

---
+++ readbookweb/test-compiled.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var http = require("http");
+http.createServer(function (request, response) {
+  var url = require('url');
+  var url_parts = url.parse(request.url, true);
+  var query = url_parts.query;
+  console.log(query);
+
+  response.writeHead(200, { "Content-Type": query.type });
+  response.write("Hello World");
+  response.end();
+}).listen(5432);
+
+//# sourceMappingURL=test-compiled.js.map
